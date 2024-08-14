c21a3d228e983d950971cb353b383a7e8b2281b4
Juliet Ebili <julietnwabunor@gmail.com>
2024-08-14 14:59:30 +0100

    added python variable, function, dictionary, list and  modules examples

---
+++ greet_module.py
@@ -0,0 +1,6 @@
+# DEfine A Function
+def greet(person):
+    return f"Good Afternoon, {person}"
+#Apply the Function
+print(greet("Juliet"))
+print(greet("Jane"))
---
+++ plp_academy.py
@@ -0,0 +1,62 @@
+# # # favorite number
+# # favorite_Number = 55
+# # print(f'My favourite number is {favorite_Number}')
+# # #String
+# # greeting = 'Hello PLP!'
+# # print(greeting)
+# # #Boolean
+# # is_python_fun = True
+# # is_python_boring = False
+# # print(f"Is python fun? {is_python_fun}")
+# # print(f'Is python boring? {is_python_boring}')
+
+# # # Control Flow
+# # speed = 80
+# # if speed > 70:
+# #     print("Fast")
+# # if speed > 40:
+# #         print("Safe")
+# # else:
+# #      print("slow")
+
+# # #Loops for $ while
+# # for i in range(8):
+# #     print(f'I love PLP Academy {i}')
+
+# # CountDown = 10
+# # while CountDown > 0:
+# #     print(f'The Count is: {CountDown}')
+# #     CountDown -= 1
+# # print("Count Down Successful!!!")
+
+# #DEfine A Function
+# # def greet(person):
+# #     return f"Good Afternoon, {person}"
+# # #Apply the Function
+# # print(greet("Juliet"))
+# # print(greet("Jane"))
+
+# #Lists and Dictionaries
+# fruits_list = ["Apple", "Cherry", "Mango"]
+# fruits_tuple = ("Bnana", "Pineapple", "Orange")
+# print(fruits_list)
+# print(fruits_tuple)
+
+# #Dictionary
+# Contacts = {
+# #     "Alice": "33220799",
+# #     "John": "34350520",
+# #     "Mark": "60000760"
+# # }
+# # print(f"Mark contact is {Contacts['Mark']}")
+
+# # # #Inbuilt modules
+# # # #Modules
+# import math
+# # use a function from module math
+# result = math.sqrt(49)
+# print(f'The square root of 49 is {result}')
+
+# #customized module
+import greet_module
+print(greet_module.greet("Jane"))
